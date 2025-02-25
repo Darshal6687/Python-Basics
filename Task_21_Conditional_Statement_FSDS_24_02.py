fc3769c580640c2339d230b96b61d8b1a34064b5
Darshal Mathukiya <mathukiyadarshal6687@gmail.com>
2025-02-25 18:39:07 +0530

    Add files via upload

---
+++ Task_21_Conditional_Statement_FSDS_24_02.py
@@ -0,0 +1,159 @@
+##################################Conditional Statement###################################################
+
+# if True:
+#     print('Data Science')
+
+# if False:
+#     print('Data Science')
+# print('Bye for Now')
+
+# if True:
+#     print('Data Science')
+# print('Bye for now')
+
+## Code - 1 Number is even or odd
+
+# x = 4
+# r= x % 2
+#
+# if r == 0:
+#     print('Even Number')
+
+# x = 5
+# r = x % 2
+#
+# if r == 0:
+#     print('Even Number')
+# print('Odd Number')
+
+# x = 8
+# r = x % 2
+#
+# if r == 0:
+#     print('Even Number')
+# print('Odd Number')
+
+
+# x = 13
+# r = x % 2
+#
+# if r == 0:
+#     print('Even Number')
+#
+# if r == 1:
+#     print('Odd Number')
+
+
+# x = 19
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#
+# if r != 0:
+#     print('odd number')
+
+# x = 3
+# r = x% 2
+#
+# if r == 0:
+#     print('Even Number')
+#     if x>5:
+#         print('Greater Number')
+#
+# else:
+#     print('Odd NUmber')
+
+# x = 4
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#     if x > 5:
+#         print('greater number')
+#
+# else:
+#     print('Odd Number')
+
+# x = 8
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#     if x > 5:
+#         print('greater number')
+#
+# else:
+#     print('Odd Number')
+
+
+# x = 4
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#     if x > 5:
+#         print('greater number')
+#     else:
+#         print('smaller number')
+#
+# else:
+#     print('Odd Number')
+
+
+
+################################ if -- elif -- else ################################################################
+
+
+# x = 9
+#
+# if x == 1:
+#     print('one')
+# elif x == 2:
+#     print('Two')
+# elif x == 3:
+#     print('Three')
+# elif x == 4:
+#     print('four')
+#
+# else:
+#     print('number not found')
+
+
+################################################ Loops #############################################
+
+# i = 1
+# while i<= 5:
+#     print('Data Science')
+#     i = i + 1
+
+
+# i = 1         # initializing
+# while i<=5:    # condition
+#     print('data science : ', i)
+#     i = i + 1  # increment
+
+# i = 5
+# while i>=1:
+#     print('Data Science :' ,i)
+#     i = i -1
+
+
+i = 1
+while i<=5:
+    print('Data Science')
+    j = 1
+    while j<=4:
+        print('Tech')
+        j = j + 1
+    i = i+1
+    print()
+
+
+
+
+
+
+
+
+
